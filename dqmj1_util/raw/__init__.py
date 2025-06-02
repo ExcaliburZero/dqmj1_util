b5ace48ad2bd43d2b345823981887e2a4d04a13e
Christopher Wells <cwellsny@nycap.rr.com>
2025-06-01 23:14:09 -0700

    Add SkillTbl classes to raw exports

--- dqmj1_util/raw/__init__.py
+++ dqmj1_util/raw/__init__.py
@@ -1,3 +1,4 @@
 from dqmj1_util.raw.btl_enmy_prm import BtlEnmyPrm, BtlEnmyPrmEntry
+from dqmj1_util.raw.skill_tbl import SkillTbl, SkillTblEntry
 
-__all__ = ["BtlEnmyPrm", "BtlEnmyPrmEntry"]
+__all__ = ["BtlEnmyPrm", "BtlEnmyPrmEntry", "SkillTbl", "SkillTblEntry"]
